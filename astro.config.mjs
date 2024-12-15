bb2462d937b6305572548dad6b156b399ceb7a72
annie buh <xannykitten@gmail.com>
2024-12-15 16:42:56 +0100

    please

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 // Import /static for a static site
-import vercelStatic from '@astrojs/vercel';
+import vercel from '@astrojs/vercel';
 export default defineConfig({
     // Must be 'static' or 'hybrid'
     site: "https://buh.moe",
     output: 'static',
-    adapter: vercelStatic({
-        webAnalytics: {
-            enabled: true,
-        }
-    }),
+    adapter: vercel(),
   });
